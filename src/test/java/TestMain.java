e3041920edb415c7473ff58ce886ec6e92d5cde5
Greg Poslov <grig.skamsk@mail.ru>
2023-02-19 09:50:18 +0500

    some data

--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -3,20 +3,20 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
-class TestMain {
-    @Test
-    void MainTest() {
-        // Given - When - Then
-        // Предусловия
-        given()
-                .baseUri("https://postman-echo.com")
-                .body("some data")
-                // Выполняемые действия
-                .when()
-                .post("/post")
-                // Проверки
-                .then()
-                .statusCode(200)
-                .body("data", equalTo("some date"));
+    class TestMain {
+        @Test
+        void MainTest() {
+            // Given - When - Then
+            // Предусловия
+            given()
+                    .baseUri("https://postman-echo.com")
+                    .body("some data")
+                    // Выполняемые действия
+                    .when()
+                    .post("/post")
+                    // Проверки
+                    .then()
+                    .statusCode(200)
+                    .body("data", equalTo("some data"));
+        }
     }
-}
