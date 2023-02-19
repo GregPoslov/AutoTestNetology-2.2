0f2fb82711dbdff0af7577b76239e67f35e31df5
Greg Poslov <grig.skamsk@mail.ru>
2023-02-19 09:45:23 +0500

    some value

--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -17,6 +17,6 @@ class TestMain {
                 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"));
+                .body("data", equalTo("some value"));
     }
 }
